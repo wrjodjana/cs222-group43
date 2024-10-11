4428c7715cdd06fb866e8746364dd05c7aaf0986
leongjanine <63428072+lumpycheese@users.noreply.github.com>
2024-10-11 13:49:17 -0500

    preferences page

--- outfit-website/src/Routes.js
+++ outfit-website/src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage";
+import { Preferences } from "./pages/Preferences";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/preferences" element={<Preferences />} />
       </Routes>
     </Router>
   );
---
+++ outfit-website/src/pages/Preferences.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { Form, Image, Button } from 'react-bootstrap';
+import AuthContext from '../context/AuthContext';
+
+
+export const Preferences = () => {
+    return (
+        <div className='user preferences'>
+            <section id="section">
+                <Form onSubmit/*={storepreferences}*/>
+                    <Form.Label className ="title" for="location"> Location </Form.Label> 
+                    <Form.Control type="location" name="location"/>
+                    <br />
+                    <Button type="submit">submit</Button>
+                </Form>
+            </section>
+        </div>
+        
+    )
+};
