f8f1c2bbf5b34efa4774a61a37f797eb7be05b33
leongjanine <63428072+lumpycheese@users.noreply.github.com>
2024-10-18 13:18:52 -0500

    Preferences page - material selection

--- outfit-website/src/pages/Preferences.js
+++ outfit-website/src/pages/Preferences.js
@@ -1,7 +1,22 @@
 import React, { useContext } from 'react';
 import { Form, Image, Button } from 'react-bootstrap';
 import AuthContext from '../context/AuthContext';
+// import React from 'react'
+import Select from 'react-select'
 
+const options = [
+  { value: 'cotton', label: 'Cotton' },
+  { value: 'linen', label: 'Linen' },
+  { value: 'polyester', label: 'Polyester' },
+  { value: 'wool', label: 'Wool' },
+  { value: 'nylon', label: 'Nylon' },
+  { value: 'denim', label: 'Denim' },
+  { value: 'jersey', label: 'Jersey' }
+]
+
+const MyComponent = () => (
+  <Select options={options} />
+)
 
 export const Preferences = () => {
     return (
@@ -10,11 +25,24 @@ export const Preferences = () => {
                 <Form onSubmit/*={storepreferences}*/>
                     <Form.Label className ="title" for="location"> Location </Form.Label> 
                     <Form.Control type="location" name="location"/>
-                    <br />
+                    <br/>
+                    <Form.Label className ="title" for="materials"> Materials </Form.Label> 
+                    {/* <Form.Control type="materials" name="materials"/> */}
+                    {/* <Select options={options} /> */}
+                    <Select
+                        isMulti
+                        name="colors"
+                        options={options}
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                    />
+                    <br/>
+                    <Form.Label className ="title" for="temperature">Ideal Temperature</Form.Label> 
+                    <Form.Control type="temperature" name="temperature"/>
+                    <br/>
                     <Button type="submit">submit</Button>
                 </Form>
             </section>
         </div>
-        
     )
 };
