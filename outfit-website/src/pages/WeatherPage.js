fadf738d73d67b028b23b9ed4c1a2359f9e09a33
wrjodjana <warrjodjana@gmail.com>
2024-10-18 14:16:37 -0500

    added styling to weather page

--- outfit-website/src/pages/WeatherPage.js
+++ outfit-website/src/pages/WeatherPage.js
@@ -1,7 +1,30 @@
+const apiKey = process.env.GOOGLE_API_KEY;
+
 export const WeatherPage = () => {
+  const divStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    height: "100vh",
+  };
+
+  const textStyle = {
+    fontSize: "32px",
+  };
+
+  const inputStyle = {
+    width: "400px",
+    height: "50px",
+    borderRadius: "20px",
+    padding: "10px 10px 10px 20px",
+    fontSize: "18px",
+  };
+
   return (
-    <div class="topnav">
-      <input type="text" placeholder="Search" />
+    <div style={divStyle} class="topnav">
+      <p style={textStyle}>Search your location!</p>
+      <input style={inputStyle} type="text" placeholder="Search" />
     </div>
   );
 };
