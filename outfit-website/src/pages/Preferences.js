ecd8ae7c1237646145c2afd42209d996b01ee7cd
wrjodjana <warrjodjana@gmail.com>
2024-10-18 14:09:52 -0500

    added styling to weather page

--- outfit-website/src/pages/Preferences.js
+++ outfit-website/src/pages/Preferences.js
@@ -1,20 +1,21 @@
-import React, { useContext } from 'react';
-import { Form, Image, Button } from 'react-bootstrap';
-import AuthContext from '../context/AuthContext';
-
+import React, { useContext } from "react";
+import { Form, Image, Button } from "react-bootstrap";
+import AuthContext from "../context/AuthContext";
 
 export const Preferences = () => {
-    return (
-        <div className='user preferences'>
-            <section id="section">
-                <Form onSubmit/*={storepreferences}*/>
-                    <Form.Label className ="title" for="location"> Location </Form.Label> 
-                    <Form.Control type="location" name="location"/>
-                    <br />
-                    <Button type="submit">submit</Button>
-                </Form>
-            </section>
-        </div>
-        
-    )
+  return (
+    <div className="user preferences">
+      <section id="section">
+        <Form onSubmit /*={storepreferences}*/>
+          <Form.Label className="title" for="location">
+            {" "}
+            Location{" "}
+          </Form.Label>
+          <Form.Control type="location" name="location" />
+          <br />
+          <Button type="submit">submit</Button>
+        </Form>
+      </section>
+    </div>
+  );
 };
