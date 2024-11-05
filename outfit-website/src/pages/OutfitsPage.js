169c853110f258cb314e64cea696cf6a736a2b71
graceren02 <148805893+graceren02@users.noreply.github.com>
2024-11-05 13:53:22 -0600

    Outfit page

---
+++ outfit-website/src/pages/OutfitsPage.js
@@ -0,0 +1,7 @@
+return (
+    <div style={divStyle} className='outfits'>
+        <section id="section">
+            <h1 style={h1style}> Let's pick out your outfit for today! </h1>
+        </section>
+    </div>
+)
